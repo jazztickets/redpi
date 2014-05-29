28ed07241efd4d82a3c205bffc2c56c0070830ef
Alan Witkowski <alan.witkowski@gmail.com>
2014-05-29 12:30:44 -0600

    added channel title to yt results blank screen when playing video

--- redpi.py
+++ redpi.py
@@ -21,7 +21,7 @@ os.makedirs(cache_path, exist_ok=True)
 os.makedirs(files_path, exist_ok=True)
 
 mode_help = [
-	"q: quit r: refresh s: subreddit y: youtube l: downloads j: up k: down enter: download",
+	"q: quit r: refresh s: subreddit y: youtube /: search l: downloads j: up k: down enter: download",
 	"q: quit r: refresh s: subreddit y: youtube l: results a: playall d: delete j: up k: down enter: play"
 ]
 
@@ -34,8 +34,10 @@ position = 0
 expire_time = 600
 results = []
 max_display = 20
+screen = None
 process = None
 menu_status = None
+menu_help = None
 scroll = 0
 mode = 0
 max_y = 0
@@ -67,16 +69,18 @@ def load_youtube(search=""):
 	i = 0
 	count_width = 2
 	date_width = 10
-	title_width = max_x - (count_width+1) - (date_width+1) - 0
-	template = "{0:%s} {1:%s} {2:%s}" % (count_width, title_width, date_width)
+	channel_width = 20
+	title_width = max_x - (count_width+1) - (channel_width+1) - (date_width+1) - 0
+	template = "{0:%s} {1:%s} {2:%s} {3:%s}" % (count_width, title_width, channel_width, date_width)
 	for item in children:
 		id = item['id']['videoId']
 		title = item['snippet']['title'][:title_width]
+		channel = item['snippet']['channelTitle'][:channel_width]
 		published_at = item['snippet']['publishedAt']
 		tdate = time.strptime(published_at, "%Y-%m-%dT%H:%M:%S.%fZ")
 
 		# build row
-		row = [str(i+1), title, time.strftime("%m-%d-%Y", tdate)]
+		row = [str(i+1), title, channel, time.strftime("%m-%d-%Y", tdate)]
 
 		data = {}
 		data['display'] = template.format(*row)
@@ -98,7 +102,7 @@ def load_subreddit(subreddit, search="", force=0):
 		else:
 			url = "http://www.reddit.com/r/" + subreddit + ".json?limit=90"
 	else:
-		url = "http://www.reddit.com/r/" + subreddit + "/search.json?limit=100&q=" + urllib.parse.quote(search) + "&restrict_sr=on"
+		url = "http://www.reddit.com/r/" + subreddit + "/search.json?limit=90&q=" + urllib.parse.quote(search) + "&restrict_sr=on"
 	
 	# load live page
 	if decoded == "": 
@@ -161,37 +165,46 @@ def load_downloads():
 		results.append(data)
 		i += 1
 
-def draw_results(menu):
+def draw_results():
+	global menu_results
 
 	if len(results) == 0:
-		menu.addstr(0, 0, "No results")
+		menu_results.addstr(0, 0, "No results")
 	else:
 		i = 0
 		for row in results[scroll : scroll + max_display]:
 			color = 1
 			if position == i:
 				color = 2
-			menu.addstr(i, 0, row['display'], curses.color_pair(color))
+			menu_results.addstr(i, 0, row['display'], curses.color_pair(color))
 			i += 1
 			if i >= max_display:
 				break
 
-	menu.noutrefresh(0, 0, 1, 0, max_y-2, max_x-1)
+	menu_results.noutrefresh(0, 0, 1, 0, max_y-2, max_x-1)
 
-def draw_help(menu):
-	menu.erase()
-	menu.addstr(0, 0, mode_help[mode][:max_x-1], curses.A_BOLD)
-	menu.noutrefresh(0, 0, 0, 0, max_y-1, max_x-1)
+def draw_help():
+	global menu_help
+	menu_help.erase()
+	menu_help.addstr(0, 0, mode_help[mode][:max_x-1], curses.A_BOLD)
+	menu_help.noutrefresh(0, 0, 0, 0, max_y-1, max_x-1)
 
 def restore_state():
+	draw_help()
+	draw_results()
 	curses.halfdelay(10)
 
 def play_video(file):
+	global screen
+
 	set_status("Playing " + file)
 	os.chdir(files_path)
 	command = play_command + " " + file
 	args = shlex.split(command)
 	try:
+		screen.clear()
+		screen.refresh()
+
 		play_process = subprocess.Popen(args, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 		play_process.wait()
 		set_status("Finished " + file)
@@ -202,7 +215,7 @@ def play_video(file):
 
 		restore_state()
 		return 1
-	
+
 	return 0
 
 def handle_selection():
@@ -278,12 +291,11 @@ def get_input(text, screen):
 	return input
 
 def main(stdscr):
-	global position, mode, max_x, max_y, scroll, menu_status, max_display
+	global position, mode, max_x, max_y, scroll, menu_status, menu_results, menu_help, max_display, screen
 
 	subreddit = "videos"
 	search = ""
 	screen = curses.initscr()
-	restore_state()
 	curses.curs_set(0)
 
 	(max_y, max_x) = screen.getmaxyx()
@@ -294,10 +306,11 @@ def main(stdscr):
 	menu_results = curses.newpad(100, 300)
 	menu_status = curses.newpad(1, 300)
 	menu_help = curses.newpad(1, 300)
+	restore_state()
 
 	load_subreddit("videos")
-	draw_results(menu_results)
-	draw_help(menu_help)
+	draw_results()
+	draw_help()
 	curses.doupdate()
 
 	while True:
@@ -406,8 +419,8 @@ def main(stdscr):
 			redraw = 1
 
 		if redraw:
-			draw_results(menu_results)
-			draw_help(menu_help)
+			draw_results()
+			draw_help()
 
 		if process != None:
 			if process.poll() == 0:
